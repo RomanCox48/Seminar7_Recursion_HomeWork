fb9a1452e6f16d7ff39b7f88fa879943f820a472
RomanKosov <kosovr48@gmail.com>
2023-12-09 22:53:30 +0500

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте произвольный массив.
+// Выведете его элементы, начиная с конца.
+// Использовать рекурсию, не использовать циклы.
+
+int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 100);
+}
+Console.WriteLine(string.Join(", ", array));
+
+void rec (int [] array, int index)
+{
+    if (index < 0)
+    {
+        return;
+    }
+    Console.Write($"{array[index]} ");
+    rec (array, index-1);
+}
+
+rec (array, array.Length-1);
